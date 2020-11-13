050c99e6f6fe998118cd30254baf6bca2e76cdb9
Mremmalex <mremmalex2@gmail.com>
2020-11-13 15:40:35 -0800

    test writing

--- writer_test.go
+++ writer_test.go
@@ -5,6 +5,6 @@ import "testing"
 func testInput(t *testing.T) {
 	want := ""
 	if got := Input(); got != want {
-		t.ErrorF("Input() = %q, want %q ", got, want)
+		t.Errorf("Input() = %q, want %q ", got, want)
 	}
 }
