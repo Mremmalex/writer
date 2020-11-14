8cec8ac1b3d03dcba011b96527c903c2afcf3f67
Mremmalex <mremmmalex2@gmail.com>
2020-11-14 14:50:30 -0800

    added a function that reads a file

--- writer.go
+++ writer.go
@@ -6,6 +6,7 @@ import (
 	"os"
 )
 
+//Input receives user input in the command line
 func Input(message ...string) string {
 	params := ":"
 	if len(message) > 0 {
@@ -16,4 +17,21 @@ func Input(message ...string) string {
 	scanner.Scan()
 	output := scanner.Text()
 	return output
+}
+
+//OpenFile takes a file name and trys to read it
+func Open(filename string) (int, interface{}) {
+	var Err interface{}
+
+	file, err := os.Open(filename)
+	var read int
+	Err = err
+	buffer := make([]byte, 100)
+	for n, e := file.Read(buffer); e == nil; n, e = file.Read(buffer) {
+		if n > 0 {
+			read, Err = os.Stdout.Write(buffer[0:n])
+			return read, Err
+		}
+	}
+	return read, Err
 }
